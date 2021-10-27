11c81743759fd0426f240af3ca3d6cacafc3a9fa
Gabriela Patricia Rodriguez <gprodriguez33@gmail.com>
2021-10-26 22:00:54 -0300

    desafio 1 JavaScript

---
+++ js/index.js
@@ -0,0 +1,16 @@
+let usuario= prompt("ingrese su nombre");
+let edad= parseInt(prompt("ingrese su edad"));
+console.log(usuario);
+console.log(edad);
+console.log("Hola " + usuario);
+console.log("Hola " + usuario + " en 5 años tendras " + (edad + 5) + " años");
+
+let nombre= "Gabriela Patricia";
+let apellido= "Rodriguez";
+console.log("yo soy " + nombre + " " + apellido);
+
+let numero1= 15;
+let numero2= 16;
+console.log(numero1 + numero2);
+console.log(numero2 - numero1);
+
